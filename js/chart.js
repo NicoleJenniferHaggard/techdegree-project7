dbb2a612aed97b1cf8001e00692f15dfa043366b
NicoleJenniferHaggard <nicolehaggard08@gmail.com>
2022-04-12 13:07:39 -0700

    fixes

--- js/chart.js
+++ js/chart.js
@@ -115,15 +115,16 @@ const user = document.getElementById("userField");
 const message = document.getElementById("messageField");
 const send = document.getElementById("send");
 
-send.addEventListener("click", () => {
+send.addEventListener("click", e => {
 
     if(user.value === "" && message.value === "") {
+        e.preventDefult();
         alert("Please fill out user and message fields before sending");
     } else if (user.value === "") {
         alert("Please fill out user field before sending");
     } else if (message.value === "") {
         alert("Please fill out message field before sending");
     } else {
-      alert("Message successfully sent to: ${user.value}");
+      alert(`Message successfully sent to: ${user.value}`);
     }
 });
